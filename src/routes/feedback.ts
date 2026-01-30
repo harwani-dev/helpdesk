9538b81218789201b5841bf8bfa990858ac8518a
Dev Harwani <dev@aubergine.co>
2026-01-30 14:48:50 +0530

    fix: giveFeedback function call

--- src/routes/feedback.ts
+++ src/routes/feedback.ts
@@ -1,22 +1,11 @@
-import { prisma } from "../lib/prisma";
 import { Router } from "express";
 import { authenticateToken, requireAdmin } from "../middleware/middleware";
-import { logActivity } from "../lib/activity";
-import { ActivityType } from "../../generated/prisma/enums";
-import { sendResponse } from "../lib/sendResponse";
-import { HTTP_STATUS } from "../constants/status";
-import { getFeedbacks } from "../controllers/feedback";
-import { createFeedbackSchema } from "../validation/feedback";
-import { UserType, TicketStatus, TicketType } from "../../generated/prisma/enums";
-import { logger } from "../utils/logger";
+import { getFeedbacks, giveFeedback } from "../controllers/feedback";
 
 const FeedbackRouter = Router();
 
 FeedbackRouter.get('/', authenticateToken, requireAdmin, getFeedbacks);
 
-
-FeedbackRouter.post('/', authenticateToken, async (req, res) => { 
-  
-});
+FeedbackRouter.post('/', authenticateToken, giveFeedback);
 
 export default FeedbackRouter;
