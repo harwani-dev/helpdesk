1e44d012d9d3a84eed2d13e845eeea29f4686b5c
Dev Harwani <dev@aubergine.co>
2026-01-30 14:54:16 +0530

    add: create activity controller

---
+++ src/controllers/activity.ts
@@ -0,0 +1,38 @@
+import type { Request, Response } from "express";
+import prisma from "../lib/prisma";
+import { sendResponse } from "../lib/sendResponse";
+import { HTTP_STATUS } from "../constants/status";
+export const getActivity = async (req: Request, res: Response) => {
+    try {
+        const activities = await prisma.activity.findMany({
+            orderBy: {
+                createdAt: "desc"
+            }
+        });
+
+        if (!activities || activities.length === 0) {
+            return sendResponse(res, HTTP_STATUS.NOT_FOUND, {
+                success: false,
+                data: null,
+                error: {
+                    code: "NO_ACTIVITIES_FOUND",
+                    details: ["No activities found"]
+                }
+            });
+        }
+        return sendResponse(res, HTTP_STATUS.OK, {
+            success: true,
+            data: activities,
+            error: null
+        })
+    } catch (error) {
+        return sendResponse(res, HTTP_STATUS.INTERNAL_SERVER_ERROR, {
+            success: false,
+            data: null,
+            error: {
+                code: "INTERNAL_SERVER_ERROR",
+                details: ["There has been an internal server error"]
+            }
+        });
+    }
+}
