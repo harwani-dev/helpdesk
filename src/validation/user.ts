e34e98eca41f22de0ef2af10eb4455e9b6683c39
Dev Harwani <dev@aubergine.co>
2026-02-11 11:44:20 +0530

    add updateSchema

--- src/validation/user.ts
+++ src/validation/user.ts
@@ -1,5 +1,10 @@
 import Joi from "joi";
 
+export const updateSchema = Joi.object({
+    profileImage: Joi.string().trim().allow("").required(),
+    phone: Joi.string().trim().length(10).pattern(/^\d+$/).allow(""),
+});
+
 export const setManagerSchema = Joi.object({
     username: Joi.string().trim().min(1).required(),
     managerUsername: Joi.string()
