000c425866447ba37071973e401569833fa6d4f5
Dev Harwani <dev@aubergine.co>
2026-02-11 11:39:10 +0530

    update: User model

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,8 @@
 import 'dotenv/config';
 import bcrypt from 'bcrypt';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../generated/prisma/client.js';
-import { UserType } from '../generated/prisma/enums.js';
+import { PrismaClient } from '@prisma/client';
+import { UserType } from '@prisma/client';
 
 const connectionString = process.env.DATABASE_URL!;
 
