0261001c52ece29037c4886b9f9cc96e1488d9c7
Dev Harwani <dev@aubergine.co>
2026-02-11 11:46:38 +0530

    update imports

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,20 @@
 import express from 'express';
-import AuthRouter from './routes/auth';
-import UserRouter from './routes/user';
-import TicketRouter from './routes/ticket';
-import FeedbackRouter from './routes/feedback';
+import AuthRouter from './routes/auth.js';
+import UserRouter from './routes/user.js';
+import TicketRouter from './routes/ticket.js';
+import FeedbackRouter from './routes/feedback.js';
 import { pinoHttp } from 'pino-http';
-import { logger } from './lib/logger';
-import { ActivityRouter } from './routes/activity';
+import { logger } from './lib/logger.js';
+import { ActivityRouter } from './routes/activity.js';
+import cors from "cors";
+
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(pinoHttp({ logger }));
+app.use(cors());
 
 app.use('/api/auth', AuthRouter);
 app.use('/api/users', UserRouter);
